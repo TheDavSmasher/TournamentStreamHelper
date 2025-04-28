73a4d21a674c06c172d5b2f9bd4a37511b626c4b
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-04-27 19:12:34 -0600

    Changed Type constraints and simplified construction

--- src/Helpers/TSHEnumHelper.py
+++ src/Helpers/TSHEnumHelper.py
@@ -7,16 +7,16 @@ class SuperEnum(Enum):
         obj._value_ = value
         return obj
 
-    def __init__(self, value, nested: EnumMeta = None):
+    def __init__(self, value, nested=None):
         self._parent_enum: SuperEnum | None = None
         if nested:
-            if isinstance(nested, EnumMeta):
-                self._nested_enum: EnumMeta = nested
-                for enm in nested:
-                    setattr(self, enm.name, enm)
-                    self._set_parent(enm)
+            if not isinstance(nested, type(self)):
+                raise TypeError(f"Nested enum must be a SuperEnum, got {type(nested)}.")
+            for enm in nested:
+                setattr(self, enm.name, enm)
+                setattr(enm, '_parent_enum', self)
 
-    def is_submember_of(self, parent_enum: EnumMeta):
+    def is_submember_of(self, parent_enum):
         return self == parent_enum
 
     def __eq__(self, other):
@@ -34,30 +34,10 @@ class SuperEnum(Enum):
             current = current._parent_enum
         return path
 
-    def _set_parent(self, enm):
-        """Recursively set parent references and modify equality/hash."""
-        setattr(enm, '_parent_enum', self)
-
-        # Define dynamic __eq__ and __hash__
-        def _eq(self_, other):
-            return self_ is other or getattr(self_, "_parent_enum") is other
-
-        def _hash(self_):
-            return hash(getattr(self_, "_parent_enum", self_))
-
-        enm.__eq__ = _eq.__get__(enm, type(enm))
-        enm.__hash__ = _hash.__get__(enm, type(enm))
-        enm.is_submember_of = _is_submember_of.__get__(enm, type(enm))
-
-        if hasattr(enm, '_nested_enum'):
-            for sub_enm in enm._nested_enum:
-                setattr(enm, sub_enm.name, sub_enm)
-                enm._set_parent(sub_enm)
-
     @property
     def top(self):
         """Return the topmost parent for this enum member."""
         current = self
-        while hasattr(current, '_parent_enum'):
+        while current._parent_enum is not None:
             current = current._parent_enum
         return current
