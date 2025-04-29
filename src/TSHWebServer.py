d5e292e4da8960f70ce700e8f8c96059fdee155b
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-04-29 13:05:59 -0600

    Added JSON marker to socket emit

--- src/TSHWebServer.py
+++ src/TSHWebServer.py
@@ -64,7 +64,7 @@ class WebServer(QThread):
 
     @socketio.on('obs_connect')
     def ws_obs_ws_connect(message):
-        emit('obs_connect', WebServer.actions.obsConnect())
+        emit('obs_connect', WebServer.actions.obsConnect(), json=True)
 
     @app.route('/ruleset')
     def ruleset():
