63686c4faa18ea12d704bc0344038f19f949f522
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-04-29 01:15:40 -0600

    Added initial setting group without setting items

--- src/OBS/TSHOBSSettingsWindow.py
+++ src/OBS/TSHOBSSettingsWindow.py
@@ -5,7 +5,15 @@ from ..Helpers.TSHSettingsHelper import GenericSettingsWindow, SettingsGroup
 
 class TSHOBSSettingsWindow(GenericSettingsWindow[OBSSettingsWidget]):
     def __init__(self, parent=None):
-        settings: list[SettingsGroup] = []
+        settings: list[SettingsGroup] = [
+            SettingsGroup(
+                "Websocket Connection",
+                OBSSettingsWidget(
+                    "obs_auth",
+                    []
+                )
+            )
+        ]
         super().__init__("OBS Websocket Settings", settings, parent=parent)
         self.selection_list = QListWidget()
         self.settings_stack = QStackedWidget()
