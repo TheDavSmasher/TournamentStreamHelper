3fa30a73f00bb8de76e62a9ab074a5ad32deeb2a
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-04-29 01:03:19 -0600

    Made abstract class inherit ABC

--- src/Helpers/TSHSettingsHelper.py
+++ src/Helpers/TSHSettingsHelper.py
@@ -1,4 +1,4 @@
-from abc import abstractmethod
+from abc import abstractmethod, ABC
 from dataclasses import dataclass
 from typing import Any, TypeVar, Generic
 
@@ -17,7 +17,7 @@ class SettingsItem:
     tooltip: str = None
 
 
-class AbstractSettingsWidget(QWidget):
+class AbstractSettingsWidget(QWidget, ABC):
     def __init__(self, settingsBase="", settings: list[SettingsItem] = []):
         super().__init__()
 
