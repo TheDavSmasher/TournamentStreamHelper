43dd55239c16b94d8c20d20d7fa07eb3a2e2d4c0
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-05-01 15:36:27 -0600

    Added Iterator to SuperEnum to get entire tree

--- src/Helpers/TSHEnumHelper.py
+++ src/Helpers/TSHEnumHelper.py
@@ -1,18 +1,28 @@
 from enum import Enum, EnumMeta
+from typing import Iterator, Type
 
 
-class SuperEnum(Enum):
+class SuperEnumMeta(EnumMeta):
+    def __iter__(cls) -> Iterator['SuperEnum']:
+        member: SuperEnum
+        for member in super().__iter__():
+            yield member
+            if member._nested is not None:
+                yield from member._nested
+
+
+class SuperEnum(Enum, metaclass=SuperEnumMeta):
     def __new__(cls, value, _=None):
         obj = object.__new__(cls)
         obj._value_ = value
         return obj
 
-    def __init__(self, _, nested: EnumMeta = None):
+    def __init__(self, _, nested: Type['SuperEnum'] = None):
         self._parent: SuperEnum | None = None
+        self._nested = nested
         if nested:
             if not issubclass(nested, SuperEnum):
                 raise TypeError(f"Nested enum must be a SuperEnum, got {type(nested)}.")
-            enm: SuperEnum
             for enm in nested:
                 setattr(self, enm.name, enm)
                 enm._parent = self
